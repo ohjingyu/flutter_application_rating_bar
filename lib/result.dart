aff38dae1ea00f87e7891972bb5c9058d898925e
ohjingyu <kyeong0644@gmail.com>
2023-08-23 16:45:51 +0900

    test

--- lib/result.dart
+++ lib/result.dart
@@ -1,6 +1,4 @@
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_rating_bar/meal_api.dart';
 
 class Result extends StatefulWidget {
   const Result({super.key});
@@ -10,19 +8,22 @@ class Result extends StatefulWidget {
 }
 
 class _ResultState extends State<Result> {
+  dynamic chartPage = const Text("차트영역");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          LineChart(LineChartData()),
-          ElevatedButton(
+          IconButton(
               onPressed: () async {
-                var mealApi = await MealApi();
-                var evalDate = DateTime.now().toString().split(' ')[0];
-                var result = await mealApi.getList(evalDate: evalDate);
+                var dt = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2023),
+                    lastDate: DateTime.now());
               },
-              child: Text('확인')),
+              icon: const Icon(Icons.calendar_month)),
+          chartPage,
         ],
       ),
     );
